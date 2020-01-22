dcdd40e563b3de647f88becfa4390ec9c12ab7c6
Mark Mariscal <markamariscal@gmail.com>
2020-01-21 17:53:23 -0800

    subset final

--- all_subsets.cpp
+++ all_subsets.cpp
@@ -1,9 +1,9 @@
 /*
  Abstract: Write a C++ program called all_subsets.cpp which displays all subsets of a set. In the problem,
- your program should read n characters from a user and display all subsets of the characters. 
- In the program, you can assume that the number of input characters is less than or equal to 15. 
- Your program should ask a user to enter the number of input characters. After that, it should read the characters. For the problem, 
- you can assume that the input characters are always distinct. 
+ your program should read n characters from a user and display all subsets of the characters.
+ In the program, you can assume that the number of input characters is less than or equal to 15.
+ Your program should ask a user to enter the number of input characters. After that, it should read the characters. For the problem,
+ you can assume that the input characters are always distinct.
  Author: Mark Mariscal
  ID: 7198
  Date: 01/21/2020
@@ -16,52 +16,55 @@
 
 using namespace std;
 
-void subsets(char *arr, char *sub ,int index){
-    if(index == strlen(arr)){
+void subsets(char *array, char *sub ,int index){
+
+    if(index == strlen(array))
+    {
         cout << "{";
-        for(int k = 0; k < strlen(sub); k++){
-            cout << sub[k];
-            if (k < strlen(sub) - 1){
+
+        for(int i = 0; i < strlen(sub); i++){
+            cout << sub[i];
+            if (i < strlen(sub) - 1){
                 cout << ",";
             }
         }
+
         cout << "}";
     } else {
         sub[index] = ' ';
-        subsets(arr, sub, index + 1);
-        sub[index] = arr[index];
-        subsets(arr, sub, index+1);
+        subsets(array, sub, index + 1);
+        sub[index] = array[index];
+        subsets(array, sub, index+1);
     }
 }
 
 
 int main()
 {
-    char *array; //pointer with array
-    int num;
-    int i;
-    char word;
+    char *array;
+    int number;
+    int k;
+    char character;
 
 
-    cout << "Number of input Character: ";
-    cin >> num;
+    cout << "Number of input characters: ";
+    cin >> number;
 
-    array = new char [num]; //array with num input
-    cout << "\nEnter " << num << " characters: " <<  endl ;
+    array = new char [number];
+    cout << "\nEnter " << number << " characters: " <<  endl ;
 
-
-    while (i < num){
-        cin >> word;
-        array[i] = word;
-        i++;
+    while (k < number){
+        cin >> character;
+        array[k] = character;
+        k++;
     }
 
-    int power = pow(2,num);
-    char *sequel = new char[power];
+    int power = pow(2,number);
+    char *set = new char[power];
 
     cout << "===== All Subsets =====" << endl;
-    subsets(array, sequel ,0);
+    subsets(array, set ,0);
     delete[] array;
-    delete[] sequel;
+    delete[] set;
     return 0;
 }
